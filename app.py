d7164b378880ad593dfa14e187f672afd38cffa6
ayu853 <ayushrajanshu53266@gmail.com>
2024-11-08 15:10:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,4 +60,5 @@ def update_quantity(name):
     return jsonify({"message": "Quantity updated successfully"}), 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
